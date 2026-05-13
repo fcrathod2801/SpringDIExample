d088c49cff2ef2cf92c483b733b17271e2c2966a
Avinash Rathod <avinash.rathod2001@gmail.com>
2026-05-13 22:06:38 +0530

    Scope Bean Example

--- src/main/java/com/anotherexample/MainExample.java
+++ src/main/java/com/anotherexample/MainExample.java
@@ -7,11 +7,11 @@ public class MainExample {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("emp.xml");
         Employee employee1= context.getBean("emp", Employee.class);
-        employee1.display();
 
+        employee1.display();
+        employee1.setDepartment("HR");
+        employee1.setEmail("asd@gmail.com");
         Employee employee2= context.getBean("emp", Employee.class);
-        employee2.setDepartment("HR");
-        employee2.setEmail("asd@gmail.com");
         employee2.display();
 
 
